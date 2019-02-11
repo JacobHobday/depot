d1ae9d88268109559dd9d8e62445d1126749bacb
JacobHobday <jacob.edward.hobday@gmail.com>
2019-02-11 15:33:41 +0000

    Enforcing DRY

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,12 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-
   # GET /products
   # GET /products.json
   def index
     @products = Product.all
   end
-
   # GET /products/1
   # GET /products/1.json
-  def show
-    @products = Product.all
-  end
-
   # GET /products/new
   def new
     @product = Product.new
